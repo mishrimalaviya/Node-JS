8daacdcc353eed1b44fbc68b711166929e65d316
mishrimalaviya <mishri1411804@gmail.com>
2025-06-10 09:48:47 +0530

    Exam

--- Mern-Stack Project/Backend/model/productSchema.js
+++ Mern-Stack Project/Backend/model/productSchema.js
@@ -1,18 +1,22 @@
 const mongoose = require("mongoose")
 const schema = mongoose.Schema({
-    name : {
-        type : String,
-        required : true
+    image: {
+        type: String,
+        required: true
     },
-    title :{
-        type : String,
-        required : true
+    name: {
+        type: String,
+        required: true
     },
-    price :{
-        type : String,
-        required : true
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
     }
 })
 
-const firstschema = mongoose.model("Product",schema)
+const firstschema = mongoose.model("Product", schema)
 module.exports = firstschema
